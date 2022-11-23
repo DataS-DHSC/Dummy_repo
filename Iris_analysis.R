ee6073bd66a9523c28e0d5de7847b5af804f037b
Sophie Brid <83339350+sophiebridonneau@users.noreply.github.com>
2022-11-23 16:46:52 +0000

    Add files via upload

---
+++ Iris_analysis.R
@@ -0,0 +1,21 @@
+
+#load in some packages
+library(dplyr)
+library(tidyverse)
+library(tidyr)
+library(ggplot2)
+
+# load in some data
+data("iris")
+head(iris)
+
+
+# clean the data
+iris_renamed <- iris %>% 
+  rename(Plants = Species)
+
+# analyse the data
+mean_petal_length <- mean(iris_renamed$Petal.Length)
+                            
+                            
+  
---
+++ mtcars_analysis.R
@@ -0,0 +1,17 @@
+#load in some packages
+library(dplyr)
+library(tidyverse)
+library(tidyr)
+library(ggplot2)
+
+# load in some data
+data("mtcars")
+head(mtcars)
+
+
+# clean the data
+mtcars_filtered <- mtcars %>% 
+  filter(vs==1)
+
+# analyse the data
+max_mpg<- max(mtcars_filtered$mpg)
